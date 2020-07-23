f9c63512d924d2423c3ae9c125da7da162b31a5f
sven <sven@endgame.com>
2020-07-23 14:55:55 -0700

    missed a thing

--- goko/src/covertree/node.rs
+++ goko/src/covertree/node.rs
@@ -84,7 +84,7 @@ impl<D: PointCloud + LabeledCloud> CoverNode<D> {
 impl<D: PointCloud + MetaCloud> CoverNode<D> {
     /// If the node has a summary attached, this returns the summary.
     pub fn metasummary(&self) -> Option<Arc<SummaryCounter<D::MetaSummary>>> {
-        self.plugins.get::<NodeLabelSummary<D::MetaSummary>>().map(|c| Arc::clone(&c.summary))
+        self.plugins.get::<NodeMetaSummary<D::MetaSummary>>().map(|c| Arc::clone(&c.summary))
     }
 }
 
